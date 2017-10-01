a6566001bede4aa98d194f65d743a78f4fac5e97
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-09-30 18:27:06 -0700

    done

--- Project 1/no1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-printf("Line 1 ..\n");
-write(1,"Line 2 ",7);
-}
--- Project 1/no2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-printf("Line 1 ..\n");
-fflush(stdout);
-write(1,"Line 2 ",7);
-}
