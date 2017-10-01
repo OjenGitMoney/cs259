320e5c7fbcf8a1f945bcf887ebf6ae55d4ddeaed
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-09-30 17:40:29 -0700

    C node added

--- Project 1/no7.c
+++ Project 1/no7.c
@@ -8,27 +8,39 @@ int	_c;
 int	_d;
 int	_e;
 int	_f;
-int childPid;
-int childPid2;
+int childPidB;
+int childPidD;
+int childPidC;
 
 	printf("\n\nBefore the fork. Process A has id : 	%d \n", getpid());
 	
-	childPid=fork();
-	if (childPid != 0){
+	childPidB=fork(); 
+	if (childPidB != 0){
 		printf("I\'m A. My pid is:	 %d \n", getpid());
-		printf("B is child. B\'s pid is:	 %d \n", childPid);
+		printf("B is child. B\'s pid is:	 %d \n", childPidB);
+		
+		childPidC=fork();
+		if (childPidC != 0){
+			printf("I\'m A. My pid is:	 %d \n", getpid());
+			printf("C is child. C\'s pid is:	 %d \n", childPidC);
+		}
+		else if (childPidC == 0){
+			printf("I am C \(child\), my id is : 	%d \n", childPidC);
+			printf("I am C, my parent\'s(A) PID : 	%d \n", getppid());
+		}
+		
 		
 	}
-	else if (childPid == 0){
-		printf("I am B \(child\), my id is : 	%d \n", childPid);
-		printf("I am B, my parent\'s PID : 	%d \n", getppid());
-		childPid2=fork();
-		if (childPid2 != 0){
+	else if (childPidB == 0){
+		printf("I am B \(child\), my id is : 	%d \n", childPidB);
+		printf("I am B, my parent\'s(A) PID : 	%d \n", getppid());
+		childPidD=fork();
+		if (childPidD != 0){
 			printf("B becoming a parent, now my PID:	 %d \n", getpid());
-			printf("B\'s child PID:	 %d \n", childPid2);
+			printf("B\'s child PID:	 %d \n", childPidD);
 		}
-		else if (childPid2 == 0){
-		printf("I am D \(child\), my id is : 	%d \n", childPid2);
+		else if (childPidD == 0){
+		printf("I am D \(child\), my id is : 	%d \n", childPidD);
 		printf("I am D, my parent (B) PID : 	%d \n", getppid());
 		}
 	}
