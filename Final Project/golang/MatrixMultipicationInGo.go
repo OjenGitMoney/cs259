907009ecce2598376a53e4a2574f5eb081de3d08
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-11-22 23:10:27 -0800

    got the go framework in place. All static values works for 3X3.

--- Final Project/golang/MatrixMultipicationInGo.go
+++ Final Project/golang/MatrixMultipicationInGo.go
@@ -8,19 +8,20 @@ import (
 
 func main() {
 	size := 3
-	mat1 := [3][3]int{}
-	mat2 := [3][3]int{}
+	//mat1 := [3][3]int{}
+	mat1 := [3][3]int{ { 4, 3, 1 }, { 3, 7, 2 }, { 4, 9, 1 } }
+	//mat2 := [3][3]int{}
+	mat2 := [3][3]int{ { 4, 10, 9 }, { 7, 10, 2 }, { 6, 4, 7 } }
+	result := [3][3]int{}
 
 	rand.Seed(int64(time.Now().Nanosecond()))
 	
-	for i:= 0; i < size; i++ {
-		for j:= 0; j < size; j++ {
-			mat1[i][j] = rand.Intn(1000) 
-			mat2[i][j] = rand.Intn(1000) 
-
-			}
-	}
-	
+	// for i:= 0; i < size; i++ {
+	// 	for j:= 0; j < size; j++ {
+	// 		mat1[i][j] = rand.Intn(10) 
+	// 		mat2[i][j] = rand.Intn(10) 
+	// 		}
+	// }
 	
 	 for i := 0; i < size; i++ {
 		for j := 0; j < size ; j++ {
@@ -40,4 +41,37 @@ func main() {
 		}	
 	
 	
+	sum := 0
+	startTime := time.Now()
+
+	for row := 0 ; row < size;  row++ {
+		for col := 0 ; col < size ; col++ {
+			
+			for k := 0 ; k < size ; k++ {
+
+                sum = sum + mat1[row][k] * mat2[k][col];
+			}
+			result[row][col] = sum;
+			sum = 0;
+		}
+			
+	}
+
+	fmt.Println()
+	fmt.Println()
+	for i := 0; i < size; i++ {
+		for j := 0; j < size ; j++ {
+			fmt.Print(result[i][j])
+			fmt.Print("\t")
+		}
+	 		fmt.Println()
+		}
+
+
+
+	//time.Sleep(time.Second * 3)
+
+	t := time.Now()
+	runTime := t.Sub(startTime)
+	fmt.Println(runTime)
 }
--- Final Project/testMatrix.c
+++ Final Project/testMatrix.c
@@ -4,30 +4,37 @@
 #include <time.h>
 #include <sys/time.h>
 #ifndef SIZE
-#define SIZE 200
+#define SIZE 3
 #endif
 
 
 
 int main(){
-int size = SIZE;
 
-int mat1[SIZE][SIZE];
-int mat2[SIZE][SIZE];
+int size = SIZE;
+// int mat1[SIZE][SIZE];
+// int mat2[SIZE][SIZE];
 int result [SIZE][SIZE];
 
-	for(int i = 0; i < size; ++i)
-	{for(int j = 0; j < size ; ++j){
-   			mat1[i][j] = rand() % 1000 +1;
-   			mat2[i][j] = rand() % 1000 +1;}}
+int mat1[SIZE][SIZE] = { {4,3,1},{3,7,2},{4,9,1} };
+int mat2[SIZE][SIZE] = { {4,10,9},{7,10,2},{6,4,7} };
 
-	 // for(int i = 0; i < size; ++i)
-	 // {for(int j = 0; j < size ; ++j){printf("%d\t",mat1[i][j]);}
-	 // 	printf("\n");}
-	 // printf("\n\n");
-	 // for(int i = 0; i < size; ++i)
-	 // {for(int j = 0; j < size ; ++j){printf("%d\t", mat2[i][j]);}
-	 // 	printf("\n");}
+
+srand ( time(NULL) );
+
+	// for(int i = 0; i < size; ++i)
+	// {for(int j = 0; j < size ; ++j){
+
+ //   			mat1[i][j] = rand() % 10 +1;
+ //   			mat2[i][j] = rand() % 10 +1;}}
+
+	 for(int i = 0; i < size; ++i)
+	 {for(int j = 0; j < size ; ++j){printf("%d\t",mat1[i][j]);}
+	 	printf("\n");}
+	 printf("\n\n");
+	 for(int i = 0; i < size; ++i)
+	 {for(int j = 0; j < size ; ++j){printf("%d\t", mat2[i][j]);}
+	 	printf("\n");}
 
 	//Doing the Multipication
 	int sum = 0;
@@ -37,6 +44,7 @@ int result [SIZE][SIZE];
 		for (int col = 0 ; col < size ; col++){
 			
 			for (int k = 0 ; k < size ; k++){
+				printf("%d\n", sum );
                 sum = sum + mat1[row][k] * mat2[k][col];
 			}
 			result[row][col] = sum;
@@ -45,10 +53,10 @@ int result [SIZE][SIZE];
 	}
 	clock_t runtime = clock() - startTime;
 	int miliSeconds = runtime * 1000 / CLOCKS_PER_SEC;
-	// printf("\n\n--------\n\n");
- //    for(int i = 0; i < size; ++i)
- //    {for(int j = 0; j < size ; ++j){printf("%d\t", result[i][j]);}
- //        printf("\n");}
+	printf("\n\n--------\n\n");
+    for(int i = 0; i < size; ++i)
+    {for(int j = 0; j < size ; ++j){printf("%d\t", result[i][j]);}
+        printf("\n");}
 
     printf("runtime : \n");
     printf("%6.5f", (double)(runtime));
