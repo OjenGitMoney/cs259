eeb031573f42fba11d54c1a661310e9b26eeda99
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-11-23 23:05:28 -0800

    the baselind 3x3 works. Now trying with 1000

--- Final Project/golang/ChannelsFromGo_MatrixMultiplication.go
+++ Final Project/golang/ChannelsFromGo_MatrixMultiplication.go
@@ -12,6 +12,9 @@ func main() {
 	mat2 := [1000][1000]int{}
 	result := [1000][1000]int{}
 
+	// mat1 := [3][3]int{ { 4, 3, 1 }, { 3, 7, 2 }, { 4, 9, 1 } }
+	// mat2 := [3][3]int{ { 4, 10, 9 }, { 7, 10, 2 }, { 6, 4, 7 } }
+	
 	rand.Seed(int64(time.Now().Nanosecond()))
 	
 	for i:= 0; i < size; i++ {
@@ -29,16 +32,31 @@ func main() {
 			
 			for k := 0 ; k < size ; k++ {
 
-                sum = sum + mat1[row][k] * mat2[k][col];
+             sum = sumMeUp(row, col, k, mat1, mat2, sum) 
 			}
 			result[row][col] = sum;
 			sum = 0;
 		}
 			
 	}
+	// fmt.Println()
+	// fmt.Println()
+
+	// for i := 0; i < size; i++ {
+	// 	for j := 0; j < size ; j++ {
+	// 		fmt.Print(result[i][j])
+	// 		fmt.Print("\t")
+	// 	}
+	//  		fmt.Println()
+	// 	}
 
 	fmt.Println()
 	t := time.Now()
 	runTime := t.Sub(startTime)
 	fmt.Println(runTime)
+}
+
+func sumMeUp(row int, col int, k int, mat1 [1000][1000]int, mat2 [1000][1000]int, sum int) int {
+	sum = sum + mat1[row][k] * mat2[k][col]
+	return sum
 }
