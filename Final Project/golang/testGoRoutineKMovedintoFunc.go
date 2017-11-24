a5a4dcf6dcc4bc72c537ec0370303b234aec3693
OjenGitMoney <ojen.goshtasb@gmail.com>
2017-11-24 11:20:27 -0800

    troubleshooting with K moved into function.
    The 3x3 returns good output. the 100 will crash

---
+++ Final Project/golang/testGoRoutineKMovedintoFunc.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main() {
+	// size := 1000
+	// mat1 := [1000][1000]int{}
+	// mat2 := [1000][1000]int{}
+	// result := [1000][1000]int{}
+
+	size := 3
+	mat1 := [3][3]int{ { 4, 3, 1 }, { 3, 7, 2 }, { 4, 9, 1 } }
+	mat2 := [3][3]int{ { 4, 10, 9 }, { 7, 10, 2 }, { 6, 4, 7 } }
+	result := [3][3]int{}
+
+	rand.Seed(int64(time.Now().Nanosecond()))
+	// for i:= 0; i < size; i++ {
+	// 	for j:= 0; j < size; j++ {
+	// 		mat1[i][j] = rand.Intn(1000) 
+	// 		mat2[i][j] = rand.Intn(1000) 
+	// 		}
+	// }
+	
+	//sum := 0
+	startTime := time.Now()
+
+	for row := 0 ; row < size;  row++ {
+		for col := 0 ; col < size ; col++ {
+			
+			result[row][col] = sumMeUp(row, col, size, mat1, mat2)
+			//sum = 0;
+		}
+			
+	}
+	fmt.Println()
+	for i := 0; i < size; i++ {
+		for j := 0; j < size ; j++ {
+			fmt.Print(result[i][j])
+			fmt.Print("\t")
+		}
+	 		fmt.Println()
+		}
+
+	fmt.Println()
+	t := time.Now()
+	runTime := t.Sub(startTime)
+	fmt.Println(runTime)
+}
+
+func sumMeUp(row int, col int, size int, mat1 [3][3]int, mat2 [3][3]int) int {
+	sum := 0
+	for k := 0 ; k < size ; k++ {
+		sum = sum + mat1[row][k] * mat2[k][col]
+	}
+	return sum
+}
